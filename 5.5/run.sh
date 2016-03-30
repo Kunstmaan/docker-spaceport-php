c2527780332dc5dbec5c5921949337f638e8b1bf
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-03-30 19:27:01 +0200

    Fix NPM issues

--- 5.5/run.sh
+++ 5.5/run.sh
@@ -3,7 +3,13 @@
 cd /app
 composer install  --optimize-autoloader --ignore-platform-reqs --env=docker
 bundle install
-npm install
+
+cp /app/package.json /root/
+cd /root && npm install -q
+rsync -rlzuIO --ignore-errors /root/node_modules/ /app/node_modules > /dev/null 2>&1
+rm -rf /root/node_modules /root/package.json
+cd /app
+
 bower install
 gulp build
 app/console assets:install --symlink --env=docker
