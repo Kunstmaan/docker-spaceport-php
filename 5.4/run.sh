8109ea6620df3f4609b36bbca2e8ccdea5e13304
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-03-30 19:21:54 +0200

    Add the docker env

--- 5.4/run.sh
+++ 5.4/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd /app
-composer install  --optimize-autoloader --ignore-platform-reqs
+composer install  --optimize-autoloader --ignore-platform-reqs --env=docker
 bundle install
 npm install
 bower install
 gulp build
-app/console assets:install --symlink
-app/console assetic:dump
+app/console assets:install --symlink --env=docker
+app/console assetic:dump --env=docker
 
 php5-fpm
