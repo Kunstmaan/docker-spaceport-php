a087473d0733099b1143b91b1d5274d5a182089e
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-04-01 16:48:19 +0200

    Fix compilation

---
+++ 5.6/configs/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /app
+composer install  --optimize-autoloader --ignore-platform-reqs
+
+cp /app/package.json /root/
+cd /root && npm install -q
+rsync -rlzuIO --ignore-errors /root/node_modules/ /app/node_modules > /dev/null 2>&1
+cd /app
+
+bundle install
+bower install --allow-root
+gulp build
+bin/console assets:install --symlink --env=docker || app/console assets:install --symlink --env=docker
+bin/console assetic:dump --env=docker || app/console assetic:dump --env=docker
+
+`$1`
