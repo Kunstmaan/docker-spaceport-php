c7cab67f9b3c085f0e1541dc3e21add353b9f69c
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-04-13 08:42:26 +0200

    Only run slow tasks when specifically requested

--- 7.0/configs/run.sh
+++ 7.0/configs/run.sh
@@ -3,28 +3,51 @@
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 
-cd /app
-if [ -f composer.json ]; then
-    composer install  --optimize-autoloader --ignore-platform-reqs
+if [ "$COMPOSER" eq "yes" ]; then
+    cd /app
+    if [ -f composer.json ]; then
+        composer install  --optimize-autoloader --ignore-platform-reqs
+    fi
 fi
 
-if [ -f package.json ]; then
-    cp /app/package.json /root/
-    cd /root && npm install -q
-    rsync -rlzuIO --ignore-errors /root/node_modules/ /app/node_modules > /dev/null 2>&1
+if [ "$NPM" eq "yes" ]; then
+    cd /app
+    if [ -f package.json ]; then
+        cp /app/package.json /root/
+        cd /root && npm install -q
+        rsync -rlzuIO --ignore-errors /root/node_modules/ /app/node_modules > /dev/null 2>&1
+    fi
 fi
-cd /app
 
-if [ -f Gemfile ]; then
-    bundle install
+if [ "$GEM" eq "yes" ]; then
+    cd /app
+    if [ -f Gemfile ]; then
+        bundle install
+    fi
 fi
-if [ -f bower.json ]; then
-    bower install --allow-root
+
+if [ "$BOWER" eq "yes" ]; then
+    cd /app
+    if [ -f bower.json ]; then
+        bower install --allow-root
+    fi
 fi
-if [ -f Gulpfile ]; then
-    gulp build
+
+if [ "$GULP" eq "yes" ]; then
+    cd /app
+    if [ -f Gulpfile ]; then
+        gulp build
+    fi
 fi
-bin/console assets:install --symlink --env=docker || app/console assets:install --symlink --env=docker
-bin/console assetic:dump --env=docker || app/console assetic:dump --env=docker
+
+if [ "$ASSETS" eq "yes" ]; then
+    cd /app
+    bin/console assets:install --symlink --env=docker || app/console assets:install --symlink --env=docker
+fi
+
+if [ "$ASSETIC" eq "yes" ]; then
+    cd /app
+    bin/console assetic:dump --env=docker || app/console assetic:dump --env=docker
+fi
 
 `$1`
